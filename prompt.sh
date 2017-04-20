c92dd4dd5d262b38acbd7fec689dafa56409abc7
andreasbehnke <ab@andreasbehnke.com>
2017-04-20 18:49:33 +0200

    shorten length of branch name

--- prompt.sh
+++ prompt.sh
@@ -5,7 +5,7 @@ find_git_branch() {
     if [[ "$branch" == "HEAD" ]]; then
       branch='detached*'
     fi
-    git_branch="($branch)"
+    git_branch="$(echo $branch | sed 's:.*/::' | cut -c1-20 )… "
   else
     git_branch=""
   fi
